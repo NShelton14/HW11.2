352f3fcad6a5a4fc238db65684f5f97fda3901ab
Nathan Shelton <nathan.shelton@student.swic.edu>
2021-04-12 02:03:59 -0500

    full project

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
+const textsize = document.getElementById('textsize');
+const textcolor = document.getElementById('textcolor');
+const textout = document.getElementById('textout');
 
+document
+  .querySelector('input[type="range"]')
+  .addEventListener('input', event => {
+    textsize.innerText = event.target.value;
+    textout.style.fontSize = `${event.target.value  }px`;
+  });
+
+document
+  .querySelector('input[type="color"]')
+  .addEventListener('input', event => {
+    textcolor.innerText = event.target.value;
+    textout.style.color = event.target.value;
+  });
+
+document
+  .querySelector('input[type="text"]')
+  .addEventListener('input', event => {
+    textout.innerText = event.target.value;
+  });
