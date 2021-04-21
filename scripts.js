fd0a8172d8b57e9ffbb817a11d4ab461986462e3
Nathan Shelton <nathan.shelton@student.swic.edu>
2021-04-21 13:39:07 -0500

    Buttons still do not work. Made this too complex.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,16 @@
 const textsize = document.getElementById('textsize');
 const textcolor = document.getElementById('textcolor');
 const p = document.createElement('p');
+const min = document.getElementById('min');
+const max = document.getElementById('max');
+const size = document.getElementById('size');
 
 document.getElementById('para').appendChild(p);
 
 p.setAttribute('id', 'textout');
 const textout = document.getElementById('textout');
+
+console.log(min, max);
 
 document
   .querySelector('input[type="range"]')
@@ -26,3 +31,12 @@ document
   .addEventListener('input', event => {
     textout.innerText = event.target.value;
   });
+
+min.addEventListener('submit', event => {
+  event.preventDefault();
+});
+
+max.addEventListener('submit', event => {
+  event.preventDefault();
+  size.max = event.target.elements[0].value;
+});
